ca4e5754d15e191c9574f8ff2b56be8a3f6ef793
carles93 <carles_12_93@hotmail.com>
2017-10-04 22:07:34 +0200

    v1

--- hello_world.java
+++ hello_world.java
@@ -1,6 +1,6 @@
 public class Hello{
 	public static void main(String[] args){
-		System.out.println("Hello World");
+		System.out.println("Hello World!!” farem que mostri el missatge “Hello World: To forgive all is as inhuman as to forgive none (Aristòtil). Fortunately we have GIT");
 	}
 }
 # hello_world
