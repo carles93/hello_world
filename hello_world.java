37b45ff18c755257333538e16ab8bffff88d088e
carles93 <32525635+carles93@users.noreply.github.com>
2017-10-04 22:45:32 +0200

    Update hello_world.java

--- hello_world.java
+++ hello_world.java
@@ -1,6 +1,5 @@
 public class Hello{
 	public static void main(String[] args){
-		System.out.println("Hello World!!” farem que mostri el missatge “Hello World: To forgive all is as inhuman as to forgive none (Aristòtil). Fortunately we have GIT");
-	}
+		System.out.println("Bonjour monde"}
 }
 # hello_world
